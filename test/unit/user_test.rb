4a8a6d7579d0b48ce95fdda7e3d42463af42e587
Bryan Wain <bryandwain@gmail.com>
2013-04-15 06:44:35 -0400

    added tests for User

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "missing displayname" do
+      user = users(:testuser)
+      user.displayname = nil
+      assert user.invalid?
+   end
+   test "too short displayname" do
+      user = users(:testuser)
+      user.displayname = "aa"
+      assert user.invalid?
+   end
+   test "good minlength displayname" do
+      user = users(:testuser)
+      user.displayname = "aaa"
+      assert user.valid?
+   end
+   test "good maxlength displayname" do
+      user = users(:testuser)
+      user.displayname = "a"*20
+      assert user.valid?
+   end
+   test "too long displayname" do
+      user = users(:testuser)
+      user.displayname = "a"*21
+      assert user.invalid?
+   end
 end
