4972ea560afe80fb74f9706fabd5063f7a9d8f0f
Bryan Wain <bryandwain@gmail.com>
2013-05-27 23:52:51 -0400

    forgot to change the serializer

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,21 +1,11 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :turn_num, :turn_status, :turn_deadline, 
-             :resources, :resource_count, :unused_development_card_count
-  has_many :development_cards, :settlements, :roads
+             :resource_count, :unused_development_card_count
+  has_many :development_cards, :settlements, :roads, :resources
   has_one :user
 
   def include_resources?
     current_user == object.user
-  end
-
-  def resources
-    retval = Hash.new
-    resource_ids = object.resources.collect { |e| e.type }
-    while !resource_ids.empty?
-      retval[resources_ids.first] = resource_ids.count(resource_ids.first)
-      resource_ids.reject!(resource_ids.first)
-    end
-    return retval
   end
 
   def development_cards
