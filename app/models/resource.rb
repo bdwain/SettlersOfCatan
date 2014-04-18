763d46051e409498cfe186f53be726eaefc7fe90
Bryan Wain <bryandwain@gmail.com>
2014-04-18 03:05:47 -0500

    make resource type mass assignable

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -4,6 +4,8 @@ class Resource < ActiveRecord::Base
   
   belongs_to :player, :inverse_of => :resources
 
+  attr_accessible :type
+
   validates_presence_of :player, :type, :count
   validates_uniqueness_of :type, :scope => :player_id
 
