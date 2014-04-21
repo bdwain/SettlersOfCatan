803d2daf8c396869c3e337a024a883ee07f522e3
bdwain <bryandwain@gmail.com>
2014-04-20 23:54:39 -0500

    add dice rolling controller and form. do model later

---
+++ app/controllers/dice_rolls_controller.rb
@@ -0,0 +1,15 @@
+class DiceRollsController < ApplicationController
+  before_filter :authenticate_user!
+
+  # POST /games
+  def create
+    player = Player.find_by_id(params[:player_id])
+    if player == nil || player.user != current_user
+      redirect_to games_url
+    elsif player.roll_dice?
+      redirect_to player.game
+    else
+      redirect_to player.game, :flash => { :error => "There was a problem rolling the dice" }
+    end
+  end
+end
---
+++ spec/controllers/dice_rolls_controller_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe DiceRollsController do
+  context "When Not Logged In" do
+    let(:game) {FactoryGirl.build_stubbed(:game)}
+    let(:player) {FactoryGirl.build_stubbed(:player)}
+    after { response.should redirect_to new_user_session_path }
+    it { post :create, :player_id => player}
+  end
+
+  context "When Logged In" do
+    login
+    describe "POST create" do
+      context "with an invalid player" do
+        before(:each) { Player.should_receive(:find_by_id).and_return(nil) }
+        it "redirects to the games index" do
+          post :create, :player_id => -1
+          response.should redirect_to(games_url)
+        end
+      end
+
+      context "with a valid player" do
+        let(:game) {FactoryGirl.build_stubbed(:game)}
+        let(:player) {FactoryGirl.build_stubbed(:player)}
+        before(:each) {player.stub(:game).and_return(game)}
+        before(:each) { Player.should_receive(:find_by_id).with(player.id.to_s).and_return(player) }
+
+        context "when the current_user does control the player" do
+          before(:each) {player.stub(:user).and_return(@current_user)}
+          context "when player.roll_dice? returns true" do
+            before(:each) do
+              player.stub(:roll_dice?).and_return(true)
+              post :create, :player_id => player
+            end
+
+            it "redirects to the game" do
+              response.should redirect_to(game)
+            end
+          end
+
+          context "when player.roll_dice? returns false" do
+            before(:each) do
+              player.stub(:roll_dice?).and_return(false)
+              post :create, :player_id => player
+            end
+
+            it "redirects to the game" do
+              response.should redirect_to(game)
+            end
+
+            it "flashes an error message" do
+              should set_the_flash[:error].to("There was a problem rolling the dice")
+            end
+          end
+        end
+
+        context "when the current_user does not control the player" do
+          let(:other_user) { FactoryGirl.build_stubbed(:confirmed_user) }
+          before(:each) {player.stub(:user).and_return(other_user)}
+
+          it "redirects to the games index" do
+            post :create, :player_id => player
+            response.should redirect_to(games_url)
+          end
+        end
+      end
+    end
+  end
+end
